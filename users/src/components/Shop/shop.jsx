24b4706687a53816ff57d4dc4cee4c4dd34c6dc0
mitej23 <mitejmadan@gmail.com>
2021-08-26 23:52:43 +0530

    added button functionality

--- users/src/components/Shop/shop.jsx
+++ users/src/components/Shop/shop.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import { data } from "../../data/data";
+
 import "./shop.styles.css";
 
-const Shop = () => {
+import { connect } from "react-redux";
+import { selectAllItems } from "../../redux/menu/menu.selectors";
+import { increment, decrement, remove } from "../../redux/index";
+
+const Shop = ({ items, increment, decrement, remove }) => {
+  console.log(items);
   return (
     <div className="shop">
       <h1>Shop</h1>
       <div className="shop-container" style={{ display: "flex" }}>
-        {data.map((item) => {
-          console.log(item.image);
+        {items.map((item) => {
           return (
             <div className="shop-item" key={item.id}>
               <img src={item.image} alt={item.name} className="image" />
               <h3>{item.name}</h3>
               <p>₹ {item.price}</p>
-              <button className="add-btn">Add to cart</button>
+              <button className="add-btn" onClick={() => increment(item.id)}>
+                Add to cart
+              </button>
             </div>
           );
         })}
@@ -23,4 +29,18 @@ const Shop = () => {
   );
 };
 
-export default Shop;
+const mapStateToProps = (state) => {
+  return {
+    items: selectAllItems(state),
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: (id) => dispatch(increment(id)),
+    decrement: (id) => dispatch(decrement(id)),
+    remove: (id) => dispatch(remove(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Shop);
