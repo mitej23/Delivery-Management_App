854d82b153ec8e7387bc6d080f7fcdd1f6a2bf5d
mitej23 <mitejmadan@gmail.com>
2021-08-27 23:40:24 +0530

    added react router

--- users/src/App.js
+++ users/src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-import Header from "./components/Header/header";
-import Shop from "./components/Shop/shop";
 
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import store from "./redux/store";
+import Home from "./pages/Home/Home";
+import Checkout from "./pages/Checkout/Checkout";
 
 function App() {
   return (
     <div>
       <Provider store={store}>
-        <Header />
-        <Shop />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/checkout" component={Checkout} />
+          </Switch>
+        </Router>
       </Provider>
     </div>
   );
