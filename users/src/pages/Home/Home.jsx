99c6e0f61687b9d80047b7af2a98a18077a1f517
mitej23 <mitejmadan@gmail.com>
2021-08-27 23:40:55 +0530

    added home component

---
+++ users/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Shop from "../../components/Shop/shop";
+import Header from "../../components/Header/header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Shop />
+    </div>
+  );
+};
+
+export default Home;
